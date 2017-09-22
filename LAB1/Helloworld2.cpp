557d2bf0385200badd804af16b9a626be8a711d2
Sau NgaiFu <610035976@qq.com>
2017-09-22 14:52:16 +0800

    Add files via upload

---
+++ LAB1/Helloworld2.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void)
+{
+	printf("Hello world1\1\nHello world2\6\nHello world3\3\n");
+
+	return 0;
+}
+
+/*
+ *Note:
+ *ASCII字符表可以输入笑脸等表情
+ *右键Project名可以设置startup project
+ */
