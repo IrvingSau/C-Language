3a4d7258e0ea5418f3f931392e83b8e5c295ebc9
Sau NgaiFu <610035976@qq.com>
2017-09-22 14:51:59 +0800

    Create Helloworld1.cpp

---
+++ LAB1/Helloworld1.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("hello world\n");/**1st**/
+	printf("hello world\n");//2rd
+	printf("hello world\n");/**3rd**/
+	return 0;
+}
+
+/*NOTE：
+ *
+ *Win32 Console Application: 32位控制台程序
+ *
+ *
+ *
+ */
+ 
